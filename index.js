e476c371c3fbe03e6ada171bb8ecd56d71614bf6
manstein <manteinvip@sina.com>
2020-04-18 14:42:07 +0800

    op

---
+++ index.js
@@ -0,0 +1,9 @@
+
+/**
+ * 从https://github.com/trending获取数据
+ * https://github.com/crazycodeboy
+ * @flow
+ */
+import gitReactNativePlugin from './trending/GitHubTrending';
+
+module.exports=gitReactNativePlugin
